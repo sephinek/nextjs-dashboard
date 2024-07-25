816b64ecd469d460c8675db4d52762962ed4c66d
sephine.k31@gmail.com <sephine.k31@gmail.com>
2024-07-24 23:02:30 -0700

    completed tutorial

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from 'next';
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Create Invoice',
+};
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            label: 'Invoices',
+            href: '/dashbaord/invoices',
+          },
+          {
+            label: 'Create Invoice',
+            href: '/dashbaord/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
